bbce4dbd6d7a30e90ba69accbbda4f0a3ac23669
Vivek Srivastava <435616+vivekfe@users.noreply.github.com>
2023-01-29 13:05:09 +0100

    Update HasPairWithSum.py

--- HasPairWithSum.py
+++ HasPairWithSum.py
@@ -17,4 +17,18 @@ input_2 = [1,2,4,4]
 print(HasPairWithSumSortedArray(input_1, 8))
 print(HasPairWithSumSortedArray(input_2, 8))
 
-def 
+def HasPairWithSumUnsortedArray(input:list, sum:int)->bool:
+    u_set=set()
+    for num in input:
+        if num in u_set:
+            return True
+        else:
+            u_set.add(sum-num)
+    return False
+
+input_1 = [1,2,3,9]
+input_2 = [1,2,4,4]
+input_3 = []
+print(HasPairWithSumUnsortedArray(input_1, 8))
+print(HasPairWithSumUnsortedArray(input_2, 8))
+print(HasPairWithSumUnsortedArray(input_3, 8))
